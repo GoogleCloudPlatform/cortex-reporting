2321c5fc9db9dda7341e9abdfecff33a0715e256
Monty Chavez <montychavez@google.com>
2024-05-15 10:33:46 -0500

    Patch 5.4.1 - Marketing, SAP Finance, SFDC & Sustainability 🌼

--- local_k9/profitcenter_hierarchy/profitcenter_node_mapping.sql
+++ local_k9/profitcenter_hierarchy/profitcenter_node_mapping.sql
@@ -1,10 +1,27 @@
-CREATE OR REPLACE PROCEDURE `{{ project_id_tgt }}.{{ dataset_reporting_tgt }}.ProfitCenterMapping`()
+-- Copyright 2024 Google LLC
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     https://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
+CREATE OR REPLACE PROCEDURE `{{ project_id_tgt }}.{{ dataset_reporting_tgt }}.ProfitCenterMapping`() --noqa:LT01
 BEGIN
   --This procedure generates table having profit center mapped to profit center hierarchy nodes.
-  DECLARE rownum INT64 DEFAULT 1;
-  DECLARE maximum_len INT64 DEFAULT NULL;
+  DECLARE max_iterations INT64 DEFAULT NULL;
 
-  CREATE OR REPLACE TABLE `{{ project_id_tgt }}.{{ dataset_reporting_tgt }}.profit_centers`(
+  SET max_iterations = (
+    SELECT MAX(level) FROM `{{ project_id_tgt }}.{{ dataset_reporting_tgt }}.profitcenter_flattened`
+  );
+
+  CREATE OR REPLACE TABLE `{{ project_id_tgt }}.{{ dataset_reporting_tgt }}.profit_centers` (
     mandt STRING,
     setclass STRING,
     subclass STRING,
@@ -13,69 +30,67 @@ BEGIN
     node STRING,
     profitcenter STRING,
     level INT64,
-    isleafnode BOOL,
-    rownumber INT64
-  );
+    isleafnode BOOL) --noqa:LT02
+  AS (
+    WITH RECURSIVE
+      ProfitHierarchy AS (
+        --Insert leaf nodes with profit center mapping.
+        SELECT
+          mandt,
+          setclass,
+          subclass,
+          hiername,
+          parent,
+          node,
+          node AS profitcenter,
+          level,
+          isleafnode,
+          0 AS current_iteration
+        FROM
+          `{{ project_id_tgt }}.{{ dataset_reporting_tgt }}.profitcenter_flattened`
+        WHERE
+          isleafnode
+          AND level = 0
 
-  --inserting leaf nodes with profit center mapping
-  INSERT INTO `{{ project_id_tgt }}.{{ dataset_reporting_tgt }}.profit_centers`
-  (mandt, setclass, subclass, hiername, parent, node, profitcenter, level, isleafnode, rownumber)
-  SELECT
-    mandt,
-    setclass,
-    subclass,
-    hiername,
-    parent,
-    node,
-    node,
-    level,
-    isleafnode,
-    rownum
-  FROM
-    `{{ project_id_tgt }}.{{ dataset_reporting_tgt }}.profitcenter_flattened`
-  WHERE isleafnode IS TRUE
-    AND level = 0;
+        UNION ALL
 
-  SET maximum_len = (
-    SELECT MAX(level) FROM `{{ project_id_tgt }}.{{ dataset_reporting_tgt }}.profitcenter_flattened`
-  );
-
-  --insert profit center mapping by parent node combination for each level
-  WHILE maximum_len != 0 DO
-    SET rownum = rownum+1;
-    INSERT INTO `{{ project_id_tgt }}.{{ dataset_reporting_tgt }}.profit_centers` --noqa: disable=L003
-    (mandt, setclass, subclass, hiername, parent, node, profitcenter, level, isleafnode, rownumber)
+        --Insert profit center mapping by parent node combination for each level.
+        SELECT
+          ProfitHierarchy.mandt,
+          ProfitHierarchy.setclass,
+          ProfitHierarchy.subclass,
+          ProfitHierarchy.hiername,
+          ProfitCenterFlattened.parent,
+          ProfitCenterFlattened.node,
+          ProfitHierarchy.profitcenter,
+          ProfitCenterFlattened.level,
+          ProfitCenterFlattened.isleafnode,
+          ProfitHierarchy.current_iteration + 1 AS current_iteration
+        FROM
+          ProfitHierarchy
+        INNER JOIN
+          `{{ project_id_tgt }}.{{ dataset_reporting_tgt }}.profitcenter_flattened`
+            AS ProfitCenterFlattened
+          ON
+            ProfitHierarchy.mandt = ProfitCenterFlattened.mandt
+            AND ProfitHierarchy.setclass = ProfitCenterFlattened.setclass
+            AND ProfitHierarchy.subclass = ProfitCenterFlattened.subclass
+            AND ProfitHierarchy.hiername = ProfitCenterFlattened.hiername
+            AND ProfitHierarchy.parent = ProfitCenterFlattened.node
+        WHERE
+          (
+            ProfitCenterFlattened.level != 0
+            OR ProfitCenterFlattened.isleafnode
+          )
+          AND ProfitHierarchy.level != 1
+          AND ProfitHierarchy.current_iteration != max_iterations --noqa:RF02
+      )
     SELECT
-      profitcentermapping.mandt,
-      profitcentermapping.setclass,
-      profitcentermapping.subclass,
-      profitcentermapping.hiername,
-      profitcenterflattened.parent,
-      profitcenterflattened.node,
-      profitcentermapping.profitcenter,
-      profitcenterflattened.level,
-      profitcenterflattened.isleafnode,
-      rownum -- noqa: disable=L027
+      * EXCEPT (current_iteration)
     FROM
-      (SELECT *
-        FROM `{{ project_id_tgt }}.{{ dataset_reporting_tgt }}.profit_centers`
-        WHERE level != 1 AND rownumber = rownum - 1) AS profitcentermapping
-    INNER JOIN
-      (SELECT * -- noqa: disable=L042
-        FROM `{{ project_id_tgt }}.{{ dataset_reporting_tgt }}.profitcenter_flattened`
-        WHERE level != 0 OR isleafnode
-      ) AS profitcenterflattened
-      ON profitcentermapping.mandt = profitcenterflattened.mandt
-        AND profitcentermapping.setclass = profitcenterflattened.setclass
-        AND profitcentermapping.subclass = profitcenterflattened.subclass
-        AND profitcentermapping.hiername = profitcenterflattened.hiername
-        AND profitcentermapping.parent = profitcenterflattened.node;
-    SET maximum_len = maximum_len - 1;
-  END WHILE;
-  --noqa: enable=all
+      ProfitHierarchy
+  );
 
-  ALTER TABLE `{{ project_id_tgt }}.{{ dataset_reporting_tgt }}.profit_centers`
-  DROP COLUMN rownumber;
 END;
 
-CALL `{{ project_id_tgt }}.{{ dataset_reporting_tgt }}.ProfitCenterMapping`();
+CALL `{{ project_id_tgt }}.{{ dataset_reporting_tgt }}.ProfitCenterMapping`(); --noqa:LT01
