c78e40d55d13365999a77238601a3989bb52d7e2
Monty Chavez <montychavez@google.com>
2024-05-15 11:00:29 -0500

    Patch 5.4.1 - Marketing, SAP Finance, SFDC & Sustainability 🌼

--- local_k9/costcenter_hierarchy/costcenter_node_mapping.sql
+++ local_k9/costcenter_hierarchy/costcenter_node_mapping.sql
@@ -1,10 +1,27 @@
-CREATE OR REPLACE PROCEDURE `{{ project_id_tgt }}.{{ dataset_reporting_tgt }}.CostCenterMapping`()
+-- Copyright 2024 Google LLC
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     https://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
+CREATE OR REPLACE PROCEDURE `{{ project_id_tgt }}.{{ dataset_reporting_tgt }}.CostCenterMapping`() --noqa:LT01
 BEGIN
   --This procedure generates table having cost center mapped to cost center hierarchy nodes.
-  DECLARE rownum INT64 DEFAULT 1;
-  DECLARE maximum_len INT64 DEFAULT NULL;
+  DECLARE max_iterations INT64 DEFAULT NULL;
 
-  CREATE OR REPLACE TABLE `{{ project_id_tgt }}.{{ dataset_reporting_tgt }}.cost_centers`(
+  SET max_iterations = (
+    SELECT MAX(level) FROM `{{ project_id_tgt }}.{{ dataset_reporting_tgt }}.costcenter_flattened`
+  );
+
+  CREATE OR REPLACE TABLE `{{ project_id_tgt }}.{{ dataset_reporting_tgt }}.cost_centers` (
     mandt STRING,
     setclass STRING,
     subclass STRING,
@@ -13,69 +30,67 @@ BEGIN
     node STRING,
     costcenter STRING,
     level INT64,
-    isleafnode BOOL,
-    rownumber INT64
-  );
+    isleafnode BOOL) --noqa:LT02
+  AS (
+    WITH RECURSIVE
+      CostHierarchy AS (
+        --Insert leaf nodes with cost center mapping.
+        SELECT
+          mandt,
+          setclass,
+          subclass,
+          hiername,
+          parent,
+          node,
+          node AS costcenter,
+          level,
+          isleafnode,
+          0 AS current_iteration
+        FROM
+          `{{ project_id_tgt }}.{{ dataset_reporting_tgt }}.costcenter_flattened`
+        WHERE
+          isleafnode
+          AND level = 0
 
-  --inserting leaf nodes with cost center mapping
-  INSERT INTO `{{ project_id_tgt }}.{{ dataset_reporting_tgt }}.cost_centers`
-  (mandt, setclass, subclass, hiername, parent, node, costcenter, level, isleafnode, rownumber)
-  SELECT
-    mandt,
-    setclass,
-    subclass,
-    hiername,
-    parent,
-    node,
-    node,
-    level,
-    isleafnode,
-    rownum
-  FROM
-    `{{ project_id_tgt }}.{{ dataset_reporting_tgt }}.costcenter_flattened`
-  WHERE isleafnode IS TRUE
-    AND level = 0;
+        UNION ALL
 
-  SET maximum_len = (
-    SELECT MAX(level) FROM `{{ project_id_tgt }}.{{ dataset_reporting_tgt }}.costcenter_flattened`
-  );
-
-  --insert cost center mapping by parent node combination for each level
-  WHILE maximum_len != 0 DO
-    SET rownum = rownum+1;
-    INSERT INTO `{{ project_id_tgt }}.{{ dataset_reporting_tgt }}.cost_centers`  --noqa: disable=L003
-    (mandt, setclass, subclass, hiername, parent, node, costcenter, level, isleafnode, rownumber)
+        --Insert cost center mapping by parent node combination for each level.
+        SELECT
+          CostHierarchy.mandt,
+          CostHierarchy.setclass,
+          CostHierarchy.subclass,
+          CostHierarchy.hiername,
+          CostCenterFlattened.parent,
+          CostCenterFlattened.node,
+          CostHierarchy.costcenter,
+          CostCenterFlattened.level,
+          CostCenterFlattened.isleafnode,
+          CostHierarchy.current_iteration + 1 AS current_iteration
+        FROM
+          CostHierarchy
+        INNER JOIN
+          `{{ project_id_tgt }}.{{ dataset_reporting_tgt }}.costcenter_flattened`
+            AS CostCenterFlattened
+          ON
+            CostHierarchy.mandt = CostCenterFlattened.mandt
+            AND CostHierarchy.setclass = CostCenterFlattened.setclass
+            AND CostHierarchy.subclass = CostCenterFlattened.subclass
+            AND CostHierarchy.hiername = CostCenterFlattened.hiername
+            AND CostHierarchy.parent = CostCenterFlattened.node
+        WHERE
+          (
+            CostCenterFlattened.level != 0
+            OR CostCenterFlattened.isleafnode
+          )
+          AND CostHierarchy.level != 1
+          AND CostHierarchy.current_iteration != max_iterations --noqa:RF02
+      )
     SELECT
-      costcentermapping.mandt,
-      costcentermapping.setclass,
-      costcentermapping.subclass,
-      costcentermapping.hiername,
-      costcenterflattened.parent,
-      costcenterflattened.node,
-      costcentermapping.costcenter,
-      costcenterflattened.level,
-      costcenterflattened.isleafnode,
-      rownum -- noqa: disable=L027
+      * EXCEPT (current_iteration)
     FROM
-      (SELECT *
-        FROM `{{ project_id_tgt }}.{{ dataset_reporting_tgt }}.cost_centers`
-        WHERE level != 1 AND rownumber = rownum - 1) AS costcentermapping
-    INNER JOIN
-      (SELECT * -- noqa: disable=L042
-        FROM `{{ project_id_tgt }}.{{ dataset_reporting_tgt }}.costcenter_flattened`
-        WHERE level != 0 OR isleafnode
-      ) AS costcenterflattened
-      ON costcentermapping.mandt = costcenterflattened.mandt
-        AND costcentermapping.setclass = costcenterflattened.setclass
-        AND costcentermapping.subclass = costcenterflattened.subclass
-        AND costcentermapping.hiername = costcenterflattened.hiername
-        AND costcentermapping.parent = costcenterflattened.node;
-    SET maximum_len = maximum_len - 1;
-  END WHILE;
-  --noqa: enable=all
+      CostHierarchy
+  );
 
-  ALTER TABLE `{{ project_id_tgt }}.{{ dataset_reporting_tgt }}.cost_centers`
-  DROP COLUMN rownumber;
 END;
 
-CALL `{{ project_id_tgt }}.{{ dataset_reporting_tgt }}.CostCenterMapping`();
+CALL `{{ project_id_tgt }}.{{ dataset_reporting_tgt }}.CostCenterMapping`(); --noqa:LT01
--- local_k9/fsv_hierarchy/fsv_glaccounts_mapping.sql
+++ local_k9/fsv_hierarchy/fsv_glaccounts_mapping.sql
@@ -1,135 +1,163 @@
-CREATE OR REPLACE PROCEDURE `{{ project_id_tgt }}.{{ dataset_reporting_tgt }}.FSVGLAccountMapping`()
+-- Copyright 2024 Google LLC
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     https://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
+CREATE OR REPLACE PROCEDURE `{{ project_id_tgt }}.{{ dataset_reporting_tgt }}.FSVGLAccountMapping`() --noqa:LT01
 BEGIN
   --This procedure generates table having glaccounts mapped to fsv hierarchy nodes.
-  DECLARE parent_node ARRAY <STRING>;
-  DECLARE glaccount_array ARRAY <STRING>;
-  DECLARE i INT64 DEFAULT 0;
-  DECLARE len INT64 DEFAULT NULL;
-  DECLARE input_parent STRING;
 
-  CREATE OR REPLACE FUNCTION `{{ project_id_tgt }}.{{ dataset_reporting_tgt }}.FindParent`(
-    input_parent STRING)
-  RETURNS STRING
-  AS ((
-      SELECT DISTINCT parent
-      FROM `{{ project_id_tgt }}.{{ dataset_reporting_tgt }}.fsv_flattened`
-      WHERE node = input_parent
-  ));
-
-  CREATE OR REPLACE TABLE `{{ project_id_tgt }}.{{ dataset_reporting_tgt }}.fsv_glaccounts`(
+  --(Re)create table and insert leaf nodes with GL Account mapping
+  CREATE OR REPLACE TABLE `{{ project_id_tgt }}.{{ dataset_reporting_tgt }}.fsv_glaccounts` (
     mandt STRING,
     chartofaccounts STRING,
     hiername STRING,
-    --noqa: disable=L008
-    {% if sql_flavour == 's4' -%} hierarchyversion STRING,
+    {% if sql_flavour == 's4' -%}
+    hierarchyversion STRING,
     {% endif -%}
     parent STRING,
     node STRING,
-    {% if sql_flavour == 'ecc' -%} ergsl STRING,
+    {% if sql_flavour == 'ecc' -%} --noqa:LT02
+    ergsl STRING, --noqa:LT02
     {% else -%}
     nodevalue STRING,
     {% endif -%}
     glaccount STRING,
-    --noqa: enable=all
     level STRING,
     isleafnode BOOL
-  );
-
-  --inserting leaf nodes with GL Account mapping
-  INSERT INTO `{{ project_id_tgt }}.{{ dataset_reporting_tgt }}.fsv_glaccounts`
-  --noqa: disable=L008
-  (mandt, chartofaccounts, hiername,
-    {% if sql_flavour == 's4' -%} hierarchyversion,
-    {% endif -%}
-    parent, node,
-    {% if sql_flavour == 'ecc' -%} ergsl,
-    {% else -%}
-    nodevalue,
-    {% endif -%}
-    glaccount, level, isleafnode)
+  ) AS
   SELECT
     mandt,
     chartofaccounts,
     hiername,
-    {% if sql_flavour == 's4' -%} hierarchyversion,
+    {% if sql_flavour == 's4' -%}
+    hierarchyversion,
     {% endif -%}
     parent,
     node,
-    {% if sql_flavour == 'ecc' -%} ergsl,
-    node,
+    {% if sql_flavour == 'ecc' -%} --noqa:disable=LT02
+    ergsl,
+    node AS glaccount,
     {% else -%}
     nodevalue,
-    nodevalue,
+    nodevalue AS glaccount,
     {% endif -%}
-    level,
+    level, --noqa: enable=all
     isleafnode
   FROM
     `{{ project_id_tgt }}.{{ dataset_reporting_tgt }}.fsv_flattened`
-  WHERE
-    isleafnode IS TRUE
-    AND node = {% if sql_flavour == 'ecc' -%} ergsl --noqa: disable=L006
+  WHERE --noqa: disable=LT02
+    isleafnode
+    {% if sql_flavour == 'ecc' -%}
+    AND node = ergsl;
     {% else -%}
-    CONCAT('1',nodevalue)
-    {% endif -%}; --noqa: enable=all
+    AND node = CONCAT('1', nodevalue);
+    {% endif -%}
+    --noqa: enable=all
 
-  --noqa: disable=L003
-  SET parent_node = ARRAY(
-    SELECT DISTINCT parent
-    FROM `{{ project_id_tgt }}.{{ dataset_reporting_tgt }}.fsv_glaccounts`);
-  SET len = ARRAY_LENGTH(parent_node);
-  --noqa: enable=all
+  -- Create temp table noting parents and relevant glaccounts
 
-  --recursively inserting GLAccounts for each non-leaf node
-  WHILE i < len DO
-  SET input_parent = parent_node[i];
-  SET glaccount_array = ARRAY(
-    SELECT DISTINCT glaccount FROM `{{ project_id_tgt }}.{{ dataset_reporting_tgt }}.fsv_glaccounts`
-    WHERE parent = input_parent);
+  CREATE TEMP TABLE GLAccountsByLeafParents AS (
+    SELECT DISTINCT
+      level,
+      parent AS leaf_parent,
+      glaccount
+    FROM
+      `{{ project_id_tgt }}.{{ dataset_reporting_tgt }}.fsv_glaccounts`
+  );
 
-    --noqa: disable=L008
-    WHILE (SELECT DISTINCT level
-      FROM `{{ project_id_tgt }}.{{ dataset_reporting_tgt }}.fsv_flattened`
-      WHERE parent = `{{ project_id_tgt }}.{{ dataset_reporting_tgt }}.FindParent`(input_parent)
-      ) != {% if sql_flavour == 'ecc' -%} '01' {% else -%} '000001'  {% endif -%}
-      DO
-      INSERT INTO `{{ project_id_tgt }}.{{ dataset_reporting_tgt }}.fsv_glaccounts`
-      (mandt, chartofaccounts, hiername,
-        {% if sql_flavour == 's4' -%}
-        hierarchyversion,
-        {% endif -%}
-        parent, node,
-        {% if sql_flavour == 'ecc' -%} ergsl,
-        {% else -%}
-        nodevalue,
-        {% endif -%}
-        glaccount, level, isleafnode)
-      SELECT
-        mandt,
-        chartofaccounts,
-        hiername,
-        {% if sql_flavour == 's4' -%} hierarchyversion,
-        {% endif -%}
-        parent,
-        node,
-        {% if sql_flavour == 'ecc' -%} ergsl,
-        {% else -%}
-        nodevalue,
-        {% endif -%}
-        glaccount, --noqa: enable=all
-        level,
-        isleafnode
+  -- 1. From leaf node, recursively gather all parents
+  -- 2. For all parents gathered, append relevant glaccounts
+  INSERT INTO `{{ project_id_tgt }}.{{ dataset_reporting_tgt }}.fsv_glaccounts`
+  (
+    mandt,
+    chartofaccounts,
+    hiername,
+    {% if sql_flavour == 's4' -%}
+    hierarchyversion,
+    {% endif -%}
+    parent,
+    node,
+    {% if sql_flavour == 'ecc' -%} --noqa:disable=LT02
+    ergsl,
+    {% else -%}
+    nodevalue,
+    {% endif -%} --noqa: enable=all
+    glaccount,
+    level,
+    isleafnode
+  )
+  WITH RECURSIVE
+    Parents AS (
+      SELECT DISTINCT
+        leaf_parent,
+        leaf_parent AS parent,
+        level
       FROM
-        `{{ project_id_tgt }}.{{ dataset_reporting_tgt }}.fsv_flattened`,
-        UNNEST (glaccount_array) AS glaccount
-        WHERE node = input_parent;
-        SET input_parent = `{{ project_id_tgt }}.{{ dataset_reporting_tgt }}.FindParent`(
-          input_parent);
-    END WHILE; -- noqa: L003
-    SET i = i + 1;
-  END WHILE;
+        GLAccountsByLeafParents
 
-  DROP FUNCTION `{{ project_id_tgt }}.{{ dataset_reporting_tgt }}.FindParent`;
+      UNION ALL
 
-  END;
+      SELECT
+        Parents.leaf_parent,
+        FSVFlattened.parent,
+        FSVFlattened.level
+      FROM
+        Parents
+      INNER JOIN `{{ project_id_tgt }}.{{ dataset_reporting_tgt }}.fsv_flattened` AS FSVFlattened
+        ON Parents.parent = FSVFlattened.node
+      WHERE
+        {% if sql_flavour == 'ecc' -%} --noqa: disable=LT02
+        FSVFlattened.level != '01'
+        {% else -%}
+        FSVFlattened.level != '000001'
+        {% endif -%}
+        --noqa: enable=all
+    ) --noqa:LT07
 
-  CALL `{{ project_id_tgt }}.{{ dataset_reporting_tgt }}.FSVGLAccountMapping`();
+  SELECT
+    FSVFlattened.mandt,
+    FSVFlattened.chartofaccounts,
+    FSVFlattened.hiername,
+    {% if sql_flavour == 's4' -%}
+    FSVFlattened.hierarchyversion,
+    {% endif -%}
+    FSVFlattened.parent,
+    FSVFlattened.node,
+    {% if sql_flavour == 'ecc' -%} --noqa:disable=LT02
+    FSVFlattened.ergsl,
+    {% else -%}
+    FSVFlattened.nodevalue,
+    {% endif -%} --noqa: enable=all
+    GLAccountsByLeafParents.glaccount,
+    FSVFlattened.level,
+    FSVFlattened.isleafnode
+  FROM
+    `{{ project_id_tgt }}.{{ dataset_reporting_tgt }}.fsv_flattened` AS FSVFlattened
+  INNER JOIN
+    Parents
+    ON
+      FSVFlattened.node = Parents.parent
+  INNER JOIN
+    GLAccountsByLeafParents
+    ON
+      Parents.leaf_parent = GLAccountsByLeafParents.leaf_parent
+  WHERE
+    {% if sql_flavour == 'ecc' -%} --noqa: disable=LT02
+    FSVFlattened.level != '01';
+    {% else -%}
+    FSVFlattened.level != '000001';
+    {% endif -%} --noqa: enable=all
+
+END;
+
+CALL `{{ project_id_tgt }}.{{ dataset_reporting_tgt }}.FSVGLAccountMapping`(); --noqa:LT01
